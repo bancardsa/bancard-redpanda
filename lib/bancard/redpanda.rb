25edc7713bdb33bdbd4bbab347539063346e6bbb
Gustavo Rodriguez <gustavo.rodriguez428@gmail.com>
2023-02-20 11:44:51 -0300

    Initial commit

---
+++ lib/bancard/redpanda.rb
@@ -0,0 +1,14 @@
+require "bancard/redpanda/version"
+
+module Bancard
+  module Redpanda
+    class Error < StandardError; end
+    # Your code goes here...
+
+    class Hola
+      def self.hi
+        puts "Hello world!"
+      end
+    end
+  end
+end
---
+++ lib/bancard/redpanda/version.rb
@@ -0,0 +1,5 @@
+module Bancard
+  module Redpanda
+    VERSION = "0.1.0"
+  end
+end
